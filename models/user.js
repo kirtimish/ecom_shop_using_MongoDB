6fc3bbd380e2a4e157c94530828b368bccf3ac9d
kirtimish <kirtimishra0708@gmail.com>
2023-01-22 03:59:31 +0530

    relations btw tables

--- models/user.js
+++ models/user.js
@@ -1,16 +1,51 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+const ObjectId = mongodb.ObjectId;
 
-const sequelize = require('../util/database');
+class User{
+  constructor(name,email,cart,id){
+      this.name = name;
+      this.email = email;
+      this.cart = cart;
+      this._id = id;
+  }
+  save(){
+      const db = getDb();
+      return db.collection('users')
+      .insertOne(this)
+  }
+  addToCart(product){
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString()
+    })
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+    if(cartProductIndex >= 0){
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push(
+        {productId: new ObjectId(product._id), quantity:1}
+      )
+    }
+
+    const updatedCart = { 
+      items: updatedCartItems
+    };
+    const db = getDb();
+    db.collection('users').updateOne(
+      {_id: new ObjectId(this._id)},
+      {$set: {cart : updatedCart}}
+    )
+  }
+  static findById(userId){
+    const db = getDb();
+    return db.collection('users')
+    .findOne({_id: new ObjectId(userId)})
+  }
+}
+
+
 
 module.exports = User;
